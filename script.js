be4669181fea0e71aa0b35734ffa3e336497ff0a
Admin <conner.p.mccabe@gmail.com>
2020-01-21 15:08:29 -0600

    initial commit

---
+++ script.js
@@ -0,0 +1,6 @@
+//basically wanted to go back and do some of these timed tests
+//I gave myself 30 minutes to do some basic DOM manipulation based on an individual assessment from SEI
+//the idea is to make a UI that has a counter, two buttons for adding or subtracting and an input for numbers
+
+
+
