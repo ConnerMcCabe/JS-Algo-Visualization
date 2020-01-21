f1fa8f1eb78a1bef87e569cd377c8b3bdd04a38a
Admin <conner.p.mccabe@gmail.com>
2020-01-21 15:24:39 -0600

    including the addition and subtraction functions

--- script.js
+++ script.js
@@ -2,5 +2,20 @@
 //I gave myself 30 minutes to do some basic DOM manipulation based on an individual assessment from SEI
 //the idea is to make a UI that has a counter, two buttons for adding or subtracting and an input for numbers
 
+let sum;
+
+let displayEl = document.querySelector('h1 span');
+let input = document.getElementById('numba');
+
+document.getElementById('add').addEventListener('click', function() {
+  sum += parseFloat(input.value)
+  render();
+});
+
+document.getElementById('subtract').addEventListener('click', function() {
+  sum -= parseFloat(input.value)
+  render();
+});
+
 
 
