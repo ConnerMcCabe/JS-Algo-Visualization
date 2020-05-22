a364331772166e5534fccbad6b6901a6858d0e30
Conner McCabe <conner.p.mccabe@gmail.com>
2020-05-22 13:24:18 -0500

    adding shapes in JS

--- script.js
+++ script.js
@@ -1,7 +1,43 @@
 //Tetris is cool
-const grid = document.querySelector('.grid')
-let squares = Array.from(document.querySelectorAll('.grid div'))
 const width = 10;
+const grid = document.querySelector('.grid')
+const ScoreDis = document.querySelector('#score')
+const StartBtn = document.querySelector('#start-button')
+let squares = Array.from(document.querySelectorAll('.grid div'))
 
-console.log(squares);
+const lTetro = [
+  [1, width + 1, width * 2 + 1, 2],
+  [width, width + 1, width + 2, width * 2 + 2],
+  [1, width + 1, width * 2 + 1, width * 2],
+  [width, width * 2, width * 2 + 1, width * 2 + 2]
+]
 
+const zTetro = [
+  [0, width, width + 1, width * 2 + 1],
+  [width + 1, width + 2, width * 2, width * 2 + 1],
+  [0, width, width + 1, width * 2 + 1],
+  [width + 1, width + 2, width * 2, width * 2 + 1]
+]
+
+const tTetro = [
+  [1, width, width + 1, width + 2],
+  [1, width + 1, width + 2, width * 2 + 1],
+  [width, width + 1, width + 2, width * 2 + 1],
+  [1, width, width + 1, width * 2 + 1]
+]
+
+const oTetro = [
+  [0, 1, width, width + 1],
+  [0, 1, width, width + 1],
+  [0, 1, width, width + 1],
+  [0, 1, width, width + 1]
+]
+
+const iTetro = [
+  [1, width + 1, width * 2 + 1, width * 3 + 1],
+  [width, width + 1, width + 2, width + 3],
+  [1, width + 1, width * 2 + 1, width * 3 + 1],
+  [width, width + 1, width + 2, width + 3]
+]
+
+const tetrominoes = [lTetro, oTetro, zTetro, iTetro, tTetro]
