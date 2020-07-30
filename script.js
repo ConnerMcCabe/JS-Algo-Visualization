41f686ecf449fb31ea268a62b065d9c84999ffe0
Conner McCabe <conner.p.mccabe@gmail.com>
2020-07-30 12:01:38 -0500

    adding doc selectors for visual elements

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 // CLEARING TETRIS OFF THE BOARD FOR CALCULATOR 
+
+const numberButtons = document.querySelectorAll('[data-number]')
+const operationButtons = document.querySelectorAll('[data-operation]')
+const equalsButton = document.querySelector('[data-equals]')
+const deleteButton = document.querySelector('[data-delete]')
+const allClearButton = document.querySelector('[data-all-clear]')
+const previousOperand = document.querySelector('[data-previous-operand]')
+const currentOperand = document.querySelector('[data-current-operand]')
