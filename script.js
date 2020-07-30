1ff484c5202494d57587ed1dcb445c4f4dc70aab
Conner McCabe <conner.p.mccabe@gmail.com>
2020-07-30 13:18:29 -0500

    current number not displaying

--- script.js
+++ script.js
@@ -1,4 +1,38 @@
 // CLEARING TETRIS OFF THE BOARD FOR CALCULATOR 
+class Calculator {
+  constructor(previousOperand, currentOperand) {
+    this.previousOperand = previousOperand
+    this.currentOperand = currentOperand
+    this.clear()
+  }
+
+  clear() {
+    this.currentOperand = ''
+    this.previousOperand = ''
+    this.operation = undefined
+  }
+
+  delete() {
+
+  }
+
+  appendNumber(number) {
+    this.currentOperand = this.currentOperand.toString() + number.toString()
+  }
+
+  chooseOperation(operation) {
+
+  }
+
+  compute() {
+
+  }
+
+  updateDisplay() {
+    this.currentOperand.innerText = this.currentOperand
+  }
+
+}
 
 const numberButtons = document.querySelectorAll('[data-number]')
 const operationButtons = document.querySelectorAll('[data-operation]')
@@ -7,3 +41,12 @@ const deleteButton = document.querySelector('[data-delete]')
 const allClearButton = document.querySelector('[data-all-clear]')
 const previousOperand = document.querySelector('[data-previous-operand]')
 const currentOperand = document.querySelector('[data-current-operand]')
+
+const calculator = new Calculator(previousOperand, currentOperand)
+
+numberButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    calculator.appendNumber(button.innerText)
+    calculator.updateDisplay()
+  })
+})
